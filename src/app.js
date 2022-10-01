e28b1bfcab488b15d925ae2f7de6d97bd0f7a503
Rafael Morais <rafahgmorais@gmail.com>
2022-10-01 15:17:37 -0300

    essentails e paragraph para o editor

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import DecoupledEditor from "@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor";
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+
 import './css/style.css';
 
-DecoupledEditor.create(document.querySelector('.document-editor__editable')).then(editor => {
+DecoupledEditor.create(document.querySelector('.document-editor__editable'), {
+    plugins: [Essentials, Paragraph],
+}).then(editor => {
     console.log(editor);
 }).catch(err => {
     console.log(err);
