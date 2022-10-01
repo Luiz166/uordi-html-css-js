59ef1b2d9b57394a1dbe174c557c32c81c21afee
Luiz166 <otavio.tata.morais@gmail.com>
2022-10-01 19:23:29 -0300

    add italic, strikethrough, underline, subscript option

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import DecoupledEditor from "@ckeditor/ckeditor5-editor-decoupled/src/decouplede
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
+import Subscript from "@ckeditor/ckeditor5-basic-styles/src/subscript";
 
 import './css/style.css';
 
 DecoupledEditor.create(document.querySelector('.document-editor__editable'), {
-    plugins: [Essentials, Paragraph, Bold],
+    plugins: [Essentials, Paragraph, Bold, Italic, Strikethrough, Underline, Subscript],
 }).then(editor => {
     console.log(editor);
     window.editor = editor;
@@ -25,6 +28,62 @@ document.querySelector("#bold").addEventListener("click", function(e){
 
     // Execute command
     editor.execute('bold');
+
+    // Restore selection
+    editor.editing.view.focus();
+    editor.model.change(writer => {
+        writer.setSelection(selectionRange);
+    });
+});
+document.querySelector("#italic").addEventListener("click", function(e){
+    e.preventDefault();
+    // Save selection
+    const selectionRange = editor.model.document.selection.getFirstRange();
+
+    // Execute command
+    editor.execute('italic');
+
+    // Restore selection
+    editor.editing.view.focus();
+    editor.model.change(writer => {
+        writer.setSelection(selectionRange);
+    });
+});
+document.querySelector("#strikethrough").addEventListener("click", function(e){
+    e.preventDefault();
+    // Save selection
+    const selectionRange = editor.model.document.selection.getFirstRange();
+
+    // Execute command
+    editor.execute('strikethrough');
+
+    // Restore selection
+    editor.editing.view.focus();
+    editor.model.change(writer => {
+        writer.setSelection(selectionRange);
+    });
+});
+document.querySelector("#underline").addEventListener("click", function(e){
+    e.preventDefault();
+    // Save selection
+    const selectionRange = editor.model.document.selection.getFirstRange();
+
+    // Execute command
+    editor.execute('underline');
+
+    // Restore selection
+    editor.editing.view.focus();
+    editor.model.change(writer => {
+        writer.setSelection(selectionRange);
+    });
+});
+document.querySelector("#subscribe").addEventListener("click", function(e){
+    e.preventDefault();
+    // Save selection
+    const selectionRange = editor.model.document.selection.getFirstRange();
+
+    // Execute command
+    editor.execute('subscript');
 
     // Restore selection
     editor.editing.view.focus();
