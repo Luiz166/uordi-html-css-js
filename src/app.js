6f798d1ccae1f0c8628aa8ffa2988c465af14d0a
Rafael Morais <rafahgmorais@gmail.com>
2022-10-01 12:47:05 -0300

    muda estilo para ficar parecido com um documento

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import DecoupledEditor from "@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor";
+import './css/style.css';
 
 DecoupledEditor.create(document.querySelector('.document-editor__editable')).then(editor => {
     console.log(editor);
